5f89a778e3abf5760a42ce2e0da0de0268078a7c
Nick Delfino <delfinonick@gmail.com>
2024-04-10 23:02:05 -0400

    Remove unused guild command register method

--- src/register.js
+++ src/register.js
@@ -7,11 +7,8 @@ import fetch from "node-fetch";
  * to be run once.
  */
 
-/* eslint-disable no-undef */
-
 const token = process.env.DISCORD_TOKEN;
 const applicationId = process.env.DISCORD_APPLICATION_ID;
-const testGuildId = process.env.DISCORD_TEST_GUILD_ID;
 
 if (!token) {
   throw new Error("The DISCORD_TOKEN environment variable is required.");
@@ -23,35 +20,9 @@ if (!applicationId) {
 }
 
 /**
- * Register all commands with a specific guild/server. Useful during initial
- * development and testing.
- */
-// eslint-disable-next-line no-unused-vars
-async function registerGuildCommands() {
-  if (!testGuildId) {
-    throw new Error(
-      "The DISCORD_TEST_GUILD_ID environment variable is required."
-    );
-  }
-  const url = `https://discord.com/api/v10/applications/${applicationId}/guilds/${testGuildId}/commands`;
-  const res = await registerCommands(url);
-  const json = await res.json();
-  console.log(json);
-  json.forEach(async (cmd) => {
-    const response = await fetch(
-      `https://discord.com/api/v10/applications/${applicationId}/guilds/${testGuildId}/commands/${cmd.id}`
-    );
-    if (!response.ok) {
-      console.error(`Problem removing command ${cmd.id}`);
-    }
-  });
-}
-
-/**
  * Register all commands globally.  This can take o(minutes), so wait until
  * you're sure these are the commands you want.
  */
-// eslint-disable-next-line no-unused-vars
 async function registerGlobalCommands() {
   const url = `https://discord.com/api/v10/applications/${applicationId}/commands`;
   await registerCommands(url);
@@ -78,4 +49,3 @@ async function registerCommands(url) {
 }
 
 await registerGlobalCommands();
-// await registerGuildCommands();
